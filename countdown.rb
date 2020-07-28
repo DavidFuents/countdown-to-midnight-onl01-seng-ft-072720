294055ed65d2ff62d13e99a89c1498029a01d507
David Fuentes <davidf0827@gmail.com>
2020-07-28 07:46:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,11 +8,11 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-# def coundown_with_sleep(number)
-#   while number > 0 
-#     puts "#{number} SECOND(S)!"
-#     number -= 1 
-#     sleep(1)
-#   end
-#   puts "HAPPY NEW YEAR!"
-# end
+def coundown_with_sleep(number)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+    sleep(1)
+  end
+  puts "HAPPY NEW YEAR!"
+end
